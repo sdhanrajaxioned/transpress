ae1cd8f0239e8e445f328e58a52edd1b78426662
PRDXN95 <dhanrajs@axioned.com>
2022-04-21 18:16:47 +0530

    added hamburger modal and accordian functionality

---
+++ assets/vendor/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+  var hamburger = $('.hamburger');
+  var navLinks = $('.nav-links');
+  var modal = $('.modal');
+  var videoContainer = $('.video-container');
+
+  //hamburger menu to show navigation links on click
+  hamburger.click(function() {
+    hamburger.toggleClass('active');
+    navLinks.toggleClass('is-active');
+    $('body').toggleClass('hide-scroll');
+  });
+
+  // plays video when modal button is clicked
+  modal.click(function(){
+    var video = $('video');
+    videoContainer.addClass('active');
+
+    if(videoContainer.hasClass('active')) {
+      video.get(0).play();
+      video.get(0).currentTime = 0;
+      videoContainer.click(function(e) {
+        if(e.target === videoContainer.get(0)){
+          videoContainer.removeClass('active');
+          video.get(0).pause();
+          video.get(0).currentTime = 0;
+        }
+      });
+      //stops video when modal close button is clicked
+      $('.modal-close').click(function() {
+        videoContainer.removeClass('active');
+        video.get(0).pause();
+        video.get(0).currentTime = 0;
+      })
+    }
+  });
+
+  // Counter functionality
+  var counterContainer = $('.counter-container');
+  $(window).scroll(function() {
+    var top_of_element = counterContainer.offset().top;
+    var bottom_of_element = counterContainer.offset().top + counterContainer.outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+      $('.target').each(function() {
+        var $this = $(this);
+        var countTo = $this.attr('data-target');
+        
+        $({ countNum: $this.text()}).animate({
+          countNum: countTo
+        },
+        {
+          duration: 1500,
+          easing:'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+        });  
+      });
+    }
+  })
+
+  // Accordian Functionality
+  $('.accordian-item:first-child').addClass('active').children('.panel').slideDown();
+  $('.accordian-item').click(function() {
+    $('div.panel').slideUp();
+    $(this).toggleClass('active');
+    if($(this).hasClass('active')){
+      $(this).siblings().removeClass('active');
+      $(this).find('div.panel').slideDown();
+    } else {
+      $(this).find('div.panel').slideUp();
+    }
+  })
+
+});
